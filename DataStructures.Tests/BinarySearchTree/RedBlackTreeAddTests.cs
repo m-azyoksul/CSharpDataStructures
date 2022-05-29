751125efd6137ff16cdb9bae7e85eba7d02059d4
Mustafa Azyoksul <m.azyoksul@gmail.com>
2022-05-29 06:31:42 +0300

    Implemented red black tree insert and tests

---
+++ DataStructures.Tests/BinarySearchTree/RedBlackTreeAddTests.cs
@@ -0,0 +1,386 @@
+﻿using DataStructures.BinarySearchTree;
+using FluentAssertions;
+using Xunit;
+
+namespace DataStructures.Tests.BinarySearchTree;
+
+public class RedBlackTreeTests
+{
+    [Fact]
+    public void Should_Add_Node_To_Empty_Tree()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+
+        // Act
+        tree.Add(4);
+
+        // Assert
+        tree.Root!.Value.Should().Be(4);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left.Should().BeNull();
+        tree.Root.Right.Should().BeNull();
+        tree.Root.Parent.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Not_Fixup()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(4);
+
+        // Act
+        tree.Add(3);
+
+        // Assert
+        tree.Root!.Value.Should().Be(4);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(3);
+        tree.Root.Left.IsBlack.Should().BeFalse();
+        tree.Root.Left.Left.Should().BeNull();
+        tree.Root.Left.Right.Should().BeNull();
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right.Should().BeNull();
+        tree.Root.Parent.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Right_Rotation()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(4);
+        tree.Add(3);
+
+        // Act
+        tree.Add(2);
+
+        // Assert
+        tree.Root!.Value.Should().Be(3);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(2);
+        tree.Root.Left.IsBlack.Should().BeFalse();
+        tree.Root.Left.Left.Should().BeNull();
+        tree.Root.Left.Right.Should().BeNull();
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right!.Value.Should().Be(4);
+        tree.Root.Right.IsBlack.Should().BeFalse();
+        tree.Root.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Should().BeNull();
+        tree.Root.Right.Parent.Should().Be(tree.Root);
+        tree.Root.Parent.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Recolor_With_Root()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(4);
+        tree.Add(3);
+        tree.Add(2);
+
+        // Act
+        tree.Add(0);
+
+        // Assert
+        tree.Root!.Value.Should().Be(3);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(2);
+        tree.Root.Left.IsBlack.Should().BeTrue();
+        tree.Root.Left.Left!.Value.Should().Be(0);
+        tree.Root.Left.Left.IsBlack.Should().BeFalse();
+        tree.Root.Left.Left.Left.Should().BeNull();
+        tree.Root.Left.Left.Right.Should().BeNull();
+        tree.Root.Left.Left.Parent.Should().Be(tree.Root.Left);
+        tree.Root.Left.Right.Should().BeNull();
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right!.Value.Should().Be(4);
+        tree.Root.Right.IsBlack.Should().BeTrue();
+        tree.Root.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Should().BeNull();
+        tree.Root.Right.Parent.Should().Be(tree.Root);
+        tree.Root.Parent.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Left_Right_Rotation()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(4);
+        tree.Add(3);
+        tree.Add(2);
+        tree.Add(0);
+
+        // Act
+        tree.Add(1);
+
+        // Assert
+        tree.Root!.Value.Should().Be(3);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(1);
+        tree.Root.Left.IsBlack.Should().BeTrue();
+        tree.Root.Left.Left!.Value.Should().Be(0);
+        tree.Root.Left.Left.IsBlack.Should().BeFalse();
+        tree.Root.Left.Left.Left.Should().BeNull();
+        tree.Root.Left.Left.Right.Should().BeNull();
+        tree.Root.Left.Left.Parent.Should().Be(tree.Root.Left);
+        tree.Root.Left.Right!.Value.Should().Be(2);
+        tree.Root.Left.Right.IsBlack.Should().BeFalse();
+        tree.Root.Left.Right.Left.Should().BeNull();
+        tree.Root.Left.Right.Right.Should().BeNull();
+        tree.Root.Left.Right.Parent.Should().Be(tree.Root.Left);
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right!.Value.Should().Be(4);
+        tree.Root.Right.IsBlack.Should().BeTrue();
+        tree.Root.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Should().BeNull();
+        tree.Root.Right.Parent.Should().Be(tree.Root);
+        tree.Root.Parent.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Right_Rotation_Then_Right_Rotation()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(7);
+        tree.Add(6);
+        tree.Add(5);
+        tree.Add(4);
+        tree.Add(3);
+        tree.Add(2);
+        tree.Add(1);
+
+        // Act
+        tree.Add(0);
+
+        // Assert
+        tree.Root!.Value.Should().Be(4);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(2);
+        tree.Root.Left.IsBlack.Should().BeFalse();
+        tree.Root.Left.Left!.Value.Should().Be(1);
+        tree.Root.Left.Left.IsBlack.Should().BeTrue();
+        tree.Root.Left.Left.Left!.Value.Should().Be(0);
+        tree.Root.Left.Left.Left.IsBlack.Should().BeFalse();
+        tree.Root.Left.Left.Left.Left.Should().BeNull();
+        tree.Root.Left.Left.Left.Right.Should().BeNull();
+        tree.Root.Left.Left.Left.Parent.Should().Be(tree.Root.Left.Left);
+        tree.Root.Left.Left.Right.Should().BeNull();
+        tree.Root.Left.Left.Parent.Should().Be(tree.Root.Left);
+        tree.Root.Left.Right!.Value.Should().Be(3);
+        tree.Root.Left.Right.IsBlack.Should().BeTrue();
+        tree.Root.Left.Right.Left.Should().BeNull();
+        tree.Root.Left.Right.Right.Should().BeNull();
+        tree.Root.Left.Right.Parent.Should().Be(tree.Root.Left);
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right!.Value.Should().Be(6);
+        tree.Root.Right.IsBlack.Should().BeFalse();
+        tree.Root.Right.Left!.Value.Should().Be(5);
+        tree.Root.Right.Left.IsBlack.Should().BeTrue();
+        tree.Root.Right.Left.Left.Should().BeNull();
+        tree.Root.Right.Left.Right.Should().BeNull();
+        tree.Root.Right.Left.Parent.Should().Be(tree.Root.Right);
+        tree.Root.Right.Right!.Value.Should().Be(7);
+        tree.Root.Right.Right.IsBlack.Should().BeTrue();
+        tree.Root.Right.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Right.Should().BeNull();
+        tree.Root.Right.Right.Parent.Should().Be(tree.Root.Right);
+        tree.Root.Right.Parent.Should().Be(tree.Root);
+        tree.Root.Parent.Should().BeNull();
+    }
+
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Left_Rotation()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(0);
+        tree.Add(1);
+
+        // Act
+        tree.Add(2);
+
+        // Assert
+        tree.Root!.Value.Should().Be(1);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(0);
+        tree.Root.Left.IsBlack.Should().BeFalse();
+        tree.Root.Left.Left.Should().BeNull();
+        tree.Root.Left.Right.Should().BeNull();
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right!.Value.Should().Be(2);
+        tree.Root.Right.IsBlack.Should().BeFalse();
+        tree.Root.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Should().BeNull();
+        tree.Root.Right.Parent.Should().Be(tree.Root);
+        tree.Root.Parent.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Recolor_With_Root2()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(0);
+        tree.Add(1);
+        tree.Add(2);
+
+        // Act
+        tree.Add(4);
+
+        // Assert
+        tree.Root!.Value.Should().Be(1);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(0);
+        tree.Root.Left.IsBlack.Should().BeTrue();
+        tree.Root.Left.Left.Should().BeNull();
+        tree.Root.Left.Right.Should().BeNull();
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right!.Value.Should().Be(2);
+        tree.Root.Right.IsBlack.Should().BeTrue();
+        tree.Root.Right.Left.Should().BeNull();
+        tree.Root.Right.Right!.Value.Should().Be(4);
+        tree.Root.Right.Right.IsBlack.Should().BeFalse();
+        tree.Root.Right.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Right.Should().BeNull();
+        tree.Root.Right.Right.Parent.Should().Be(tree.Root.Right);
+        tree.Root.Right.Parent.Should().Be(tree.Root);
+        tree.Root.Parent.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Right_Left_Rotation()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(0);
+        tree.Add(1);
+        tree.Add(2);
+        tree.Add(4);
+
+        // Act
+        tree.Add(3);
+
+        // Assert
+        tree.Root!.Value.Should().Be(1);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(0);
+        tree.Root.Left.IsBlack.Should().BeTrue();
+        tree.Root.Left.Left.Should().BeNull();
+        tree.Root.Left.Right.Should().BeNull();
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right!.Value.Should().Be(3);
+        tree.Root.Right.IsBlack.Should().BeTrue();
+        tree.Root.Right.Left!.Value.Should().Be(2);
+        tree.Root.Right.Left.IsBlack.Should().BeFalse();
+        tree.Root.Right.Left.Left.Should().BeNull();
+        tree.Root.Right.Left.Right.Should().BeNull();
+        tree.Root.Right.Left.Parent.Should().Be(tree.Root.Right);
+        tree.Root.Right.Right!.Value.Should().Be(4);
+        tree.Root.Right.Right.IsBlack.Should().BeFalse();
+        tree.Root.Right.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Right.Should().BeNull();
+        tree.Root.Right.Right.Parent.Should().Be(tree.Root.Right);
+        tree.Root.Right.Parent.Should().Be(tree.Root);
+        tree.Root.Parent.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Recolor_Without_Root()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(0);
+        tree.Add(1);
+        tree.Add(2);
+        tree.Add(4);
+        tree.Add(3);
+
+        // Act
+        tree.Add(5);
+
+        // Assert
+        tree.Root!.Value.Should().Be(1);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(0);
+        tree.Root.Left.IsBlack.Should().BeTrue();
+        tree.Root.Left.Left.Should().BeNull();
+        tree.Root.Left.Right.Should().BeNull();
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right!.Value.Should().Be(3);
+        tree.Root.Right.IsBlack.Should().BeFalse();
+        tree.Root.Right.Left!.Value.Should().Be(2);
+        tree.Root.Right.Left.IsBlack.Should().BeTrue();
+        tree.Root.Right.Left.Left.Should().BeNull();
+        tree.Root.Right.Left.Right.Should().BeNull();
+        tree.Root.Right.Left.Parent.Should().Be(tree.Root.Right);
+        tree.Root.Right.Right!.Value.Should().Be(4);
+        tree.Root.Right.Right.IsBlack.Should().BeTrue();
+        tree.Root.Right.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Right!.Value.Should().Be(5);
+        tree.Root.Right.Right.Right.IsBlack.Should().BeFalse();
+        tree.Root.Right.Right.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Right.Right.Should().BeNull();
+        tree.Root.Right.Right.Right.Parent.Should().Be(tree.Root.Right.Right);
+        tree.Root.Right.Right.Parent.Should().Be(tree.Root.Right);
+        tree.Root.Right.Parent.Should().Be(tree.Root);
+        tree.Root.Parent.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_Add_Node_And_Do_Left_Rotation_Then_Left_Rotation()
+    {
+        // Arrange
+        var tree = new RedBlackTree<int>();
+        tree.Add(0);
+        tree.Add(1);
+        tree.Add(2);
+        tree.Add(3);
+        tree.Add(4);
+        tree.Add(5);
+        tree.Add(6);
+
+        // Act
+        tree.Add(7);
+
+        // Assert
+        tree.Root!.Value.Should().Be(3);
+        tree.Root.IsBlack.Should().BeTrue();
+        tree.Root.Left!.Value.Should().Be(1);
+        tree.Root.Left.IsBlack.Should().BeFalse();
+        tree.Root.Left.Left!.Value.Should().Be(0);
+        tree.Root.Left.Left.IsBlack.Should().BeTrue();
+        tree.Root.Left.Left.Left.Should().BeNull();
+        tree.Root.Left.Left.Right.Should().BeNull();
+        tree.Root.Left.Left.Parent.Should().Be(tree.Root.Left);
+        tree.Root.Left.Right!.Value.Should().Be(2);
+        tree.Root.Left.Right.IsBlack.Should().BeTrue();
+        tree.Root.Left.Right.Left.Should().BeNull();
+        tree.Root.Left.Right.Right.Should().BeNull();
+        tree.Root.Left.Right.Parent.Should().Be(tree.Root.Left);
+        tree.Root.Left.Parent.Should().Be(tree.Root);
+        tree.Root.Right!.Value.Should().Be(5);
+        tree.Root.Right.IsBlack.Should().BeFalse();
+        tree.Root.Right.Left!.Value.Should().Be(4);
+        tree.Root.Right.Left.IsBlack.Should().BeTrue();
+        tree.Root.Right.Left.Left.Should().BeNull();
+        tree.Root.Right.Left.Right.Should().BeNull();
+        tree.Root.Right.Left.Parent.Should().Be(tree.Root.Right);
+        tree.Root.Right.Right!.Value.Should().Be(6);
+        tree.Root.Right.Right.IsBlack.Should().BeTrue();
+        tree.Root.Right.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Right!.Value.Should().Be(7);
+        tree.Root.Right.Right.Right.IsBlack.Should().BeFalse();
+        tree.Root.Right.Right.Right.Left.Should().BeNull();
+        tree.Root.Right.Right.Right.Right.Should().BeNull();
+        tree.Root.Right.Right.Right.Parent.Should().Be(tree.Root.Right.Right);
+        tree.Root.Right.Right.Parent.Should().Be(tree.Root.Right);
+        tree.Root.Right.Parent.Should().Be(tree.Root);
+        tree.Root.Parent.Should().BeNull();
+    }
+}
--- DataStructures/BinarySearchTree/RedBlackTree.cs
+++ DataStructures/BinarySearchTree/RedBlackTree.cs
@@ -6,10 +6,18 @@ public class RedBlackTreeNode<T> : BstNode<T, RedBlackTreeNode<T>>
 {
     public RedBlackTreeNode(T value) : base(value)
     {
-        IsRed = true;
+        IsBlack = true;
     }
 
-    public bool IsRed { get; set; }
+    public RedBlackTreeNode(T value, RedBlackTreeNode<T> parent) : base(value)
+    {
+        Parent = parent;
+        IsBlack = false;
+    }
+
+    public bool IsBlack { get; set; }
+
+    public RedBlackTreeNode<T>? Parent { get; set; }
 }
 
 /// <summary>
@@ -21,18 +29,182 @@ public class RedBlackTreeNode<T> : BstNode<T, RedBlackTreeNode<T>>
 /// - Red nodes cannot have red children.
 /// - Every path to a leaf node contains the same number of black nodes.
 /// </summary>
-/// <typeparam name="T"></typeparam>
+/// <typeparam name="T">Node value type</typeparam>
 public class RedBlackTree<T> : Bst<T, RedBlackTreeNode<T>>
     where T : IComparable<T>
 {
     public override void Add(T value)
     {
-        throw new System.NotImplementedException();
+        if (Root == null)
+        {
+            Root = new RedBlackTreeNode<T>(value);
+            return;
+        }
+
+        // Insert the new node
+        var current = Root;
+        while (true)
+        {
+            if (value.CompareTo(current.Value) <= 0)
+            {
+                if (current.Left == null)
+                {
+                    current.Left = new RedBlackTreeNode<T>(value, current);
+                    current = current.Left;
+                    break;
+                }
+
+                current = current.Left;
+            }
+            else
+            {
+                if (current.Right == null)
+                {
+                    current.Right = new RedBlackTreeNode<T>(value, current);
+                    current = current.Right;
+                    break;
+                }
+
+                current = current.Right;
+            }
+        }
+
+        // Insert fix-up
+        while (current.Parent is {IsBlack: false}) // Parent is not black which means it is not root.
+        {
+            if (current.Parent == current.Parent.Parent!.Left) // If the parent is a left child
+            {
+                var uncle = current.Parent.Parent.Right;
+                if (uncle is {IsBlack: false}) // Case 1: Uncle is red
+                {
+                    current.Parent.IsBlack = true;
+                    uncle.IsBlack = true;
+                    current.Parent.Parent.IsBlack = false;
+                    current = current.Parent.Parent;
+                }
+                else // Case 2: Uncle is black
+                {
+                    if (current == current.Parent.Right) // Case 2.1: If the node is a right child
+                    {
+                        current = current.Parent;
+                        LeftRotate(current);
+                    }
+
+                    // Case 2.2: If the node is a left child
+                    current.Parent!.IsBlack = true;
+                    current.Parent.Parent!.IsBlack = false;
+                    RightRotate(current.Parent.Parent);
+                }
+            }
+            else // If the parent is a right child
+            {
+                var uncle = current.Parent.Parent.Left;
+                if (uncle is {IsBlack: false}) // Case 1: Uncle is red
+                {
+                    current.Parent.IsBlack = true;
+                    uncle.IsBlack = true;
+                    current.Parent.Parent.IsBlack = false;
+                    current = current.Parent.Parent;
+                }
+                else // Case 2: Uncle is black
+                {
+                    if (current == current.Parent.Left) // Case 2.1: If the node is a left child
+                    {
+                        current = current.Parent;
+                        RightRotate(current);
+                    }
+
+                    // Case 2.2: If the node is a right child
+                    current.Parent!.IsBlack = true;
+                    current.Parent.Parent!.IsBlack = false;
+                    LeftRotate(current.Parent.Parent);
+                }
+            }
+        }
+        
+        Root.IsBlack = true;
+    }
+
+    private void LeftRotate(RedBlackTreeNode<T> node)
+    {
+        // Update root
+        if (node == Root)
+            Root = node.Right;
+
+        // Connect parent and right child
+        node.Right!.Parent = node.Parent;
+        if (node.Parent == null)
+        {
+        }
+        else if (node == node.Parent.Left)
+            node.Parent.Left = node.Right;
+        else
+            node.Parent.Right = node.Right;
+
+        // Set node's right child as parent
+        node.Parent = node.Right;
+
+        // Connect node and right child's left child
+        node.Right = node.Right.Left;
+        if (node.Right != null)
+            node.Right.Parent = node;
+
+        // Set new root's left child as node
+        node.Parent.Left = node;
+    }
+
+    private void RightRotate(RedBlackTreeNode<T> node)
+    {
+        // Update root
+        if (node == Root)
+            Root = node.Left;
+
+        // Connect parent and left child
+        node.Left!.Parent = node.Parent;
+        if (node.Parent == null)
+        {
+        }
+        else if (node == node.Parent.Right)
+            node.Parent.Right = node.Left;
+        else
+            node.Parent.Left = node.Left;
+
+        // Set node's left child as parent
+        node.Parent = node.Left;
+
+        // Connect node and left child's right child
+        node.Left = node.Left.Right;
+        if (node.Left != null)
+            node.Left.Parent = node;
+
+        // Set new root's right child as node
+        node.Parent.Right = node;
     }
 
     public override T PopMin()
     {
-        throw new System.NotImplementedException();
+        if (Root == null)
+            throw new InvalidOperationException("The tree is empty");
+        if (Root.Left == null)
+        {
+            var value = Root.Value;
+            Root = Root.Right;
+            if (Root != null)
+                Root.Parent = null;
+            return value;
+        }
+
+        var current = Root.Left;
+        while (current.Left != null)
+            current = current.Left;
+
+        if (!current.IsBlack)
+        {
+            current.Parent!.Left = null;
+        }
+
+        //Remove(current);
+        return current.Value;
     }
 
     public override T PopMax()
